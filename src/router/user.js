e1ac05ba6e2fc55f32f1dcc945f58cc46f7039d0
lixinj <mao788380247@yahoo.com>
2020-12-11 15:10:21 +0800

    blog api login

--- src/router/user.js
+++ src/router/user.js
@@ -1,11 +1,16 @@
+const { loginCheck } =require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
 const handleUserRouter = (req,res) => {
     const method = req.method
-    const url = req.url
-    const path = url.split('?')[0]
 
-    if(method === "GET" && url === "/api/user/login"){
-        return {
-            msg: "这是用户登录接口"
+    if(method === "POST" && req.path === "/api/user/login"){
+        const { username, password } = req.body
+        const result = loginCheck(username, password)
+        if ( result ){
+            return new SuccessModel('登陆成功')
+        } else {
+            return new ErrorModel('登陆失败')
         }
     }
 }
