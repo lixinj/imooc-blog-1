6ad1db02095f5937518bdef26719125282be4930
lixinj <mao788380247@yahoo.com>
2020-12-17 23:03:54 +0800

    db_connect api list

--- app.js
+++ app.js
@@ -46,11 +46,21 @@ const serverHandle = (req,res) => {
         req.body = postData
 
         // 处理 blog 路由
-        const blogData = handleBlogRouter(req,res)
-        if(blogData){
-            res.end(
-                JSON.stringify(blogData)
-            )
+        // const blogData = handleBlogRouter(req,res)
+        // if(blogData){
+        //     res.end(
+        //         JSON.stringify(blogData)
+        //     )
+        //     return
+        // }
+
+        const blogResult = handleBlogRouter(req,res)
+        if(blogResult){
+            blogResult.then(blogData => {
+                res.end(
+                    JSON.stringify(blogData)
+                )
+            })
             return
         }
 
--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,21 +1,17 @@
+const { exec } = require('../db/mysql')
+
 const getList = (author, keyword) => {
-    //先返回假数据（格式是正确）
-    return [
-        {
-            id: 1,
-            title: '标题A',
-            content: '内容A',
-            createTime: 1607430861141,
-            author: 'zhangsan'
-        },
-        {
-            id: 1,
-            title: '标题B',
-            content: '内容B',
-            createTime: 1607430903397,
-            author: 'lisi'
-        }
-    ]
+    let sql = `select * from blogs where 1=1 `
+    if( author ) {
+        sql += `and author = '${author}' `
+    }
+    if( keyword ) {
+        sql += `and title like '%${keyword}%' `
+    }
+    
+    sql += `order by createtime desc;`
+    
+    return exec(sql);
 }
 
 const getDetail = (id) => {
@@ -23,7 +19,7 @@ const getDetail = (id) => {
         id: 1,
         title: '标题A',
         content: '内容A',
-        createTime: 1607430861141,
+        createTime: 1607430861141, 
         author: 'zhangsan'
     }
 }
