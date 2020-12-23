9ad2d70eaace57628c6d72e8930457a211ba32cd
lixinj <mao788380247@yahoo.com>
2020-12-23 19:13:05 +0800

    api login test

--- src/router/user.js
+++ src/router/user.js
@@ -1,18 +1,28 @@
-const { loginCheck } =require('../controller/user')
+const { login } =require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const handleUserRouter = (req,res) => {
     const method = req.method
 
+    // 登陆
     if(method === "POST" && req.path === "/api/user/login"){
         const { username, password } = req.body
-        const result = loginCheck(username, password)
+        const result = login(username, password)
         return result.then(data => {            
-            if ( data.username ){
+            if ( data.username ){                                
+
                 return new SuccessModel('登陆成功')
             } 
             return new ErrorModel('登陆失败')            
         })
+    }
+
+    // 登陆验证
+    if(method === 'GET' && req.path === '/api/user/login-test'){
+        if(req.cookie.username){
+            return Promise.resolve(new SuccessModel('已登录'))
+        }
+        return  Promise.resolve(new ErrorModel('未登录'))
     }
 }
 
