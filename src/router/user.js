a4119eb10f285ca1c707a5f7e6e696607ed58b8e
lixinj <mao788380247@yahoo.com>
2020-12-23 19:24:41 +0800

    setCookie and test

--- src/router/user.js
+++ src/router/user.js
@@ -5,11 +5,13 @@ const handleUserRouter = (req,res) => {
     const method = req.method
 
     // 登陆
-    if(method === "POST" && req.path === "/api/user/login"){
-        const { username, password } = req.body
+    if(method === "GET" && req.path === "/api/user/login"){
+        const { username, password } = req.query
         const result = login(username, password)
         return result.then(data => {            
             if ( data.username ){                                
+
+                res.setHeader('Set-Cookie', `username = ${data.username}; path=/`)
 
                 return new SuccessModel('登陆成功')
             } 
